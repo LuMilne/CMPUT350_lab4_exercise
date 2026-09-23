214fd7a3c6de3dffc4901f19bbfa190cf29c572a
Lucas Milne <lemilne@ualberta.ca>
2026-09-22 22:03:42 -0600

    p2b WIP

--- src/p2a.h
+++ src/p2a.h
@@ -2,20 +2,18 @@
 #include <cassert>
 //#include <iostream>
 //#include <bitset>
-
-
 /*
 Source: https://stackoverflow.com/questions/3203764/bit-twiddling-hacks-interleave-bits-the-obvious-way 
 Accessed: 09/22/2026 4:00
 Note: Function made by referencing "Obvious" Algorithm from user polygenelubricants
 */
-uint64_t expand(uint64_t input, uint32_t scale) {
+inline uint64_t expand(uint64_t input, uint32_t scale) {
     assert(scale > 1);
     uint64_t output = 0;
     // Iterate over bits in the input
     for(int i = 0; i < sizeof(uint64_t); i++) {
         // Check if bit is 1 or 0
-        uint64_t check = input & (1 << i); // Can only have a 1 in the ith bit from the right
+        uint64_t check = input & (static_cast<uint64_t>(1) << i); // Can only have a 1 in the ith bit from the right
         // Shift the bit to its new position
         check = (check << (scale-1)*i);
         //std::bitset<16> show(check);
@@ -29,6 +27,8 @@ inline uint64_t morton3d(uint64_t x, uint64_t y, uint64_t z) {
     uint64_t output = 0;
     // Iterate over the bits in the inputs
     for(int i = 0; i < sizeof(uint64_t); i++) {
+        /* Reduce space usage
+        ---------------------------------------------
         // Retrieve bit at ith location from the right
         uint64_t x_shift = x & (1 << i);
         uint64_t y_shift = y & (1 << i);
@@ -39,6 +39,10 @@ inline uint64_t morton3d(uint64_t x, uint64_t y, uint64_t z) {
         z_shift = z_shift << i*2+2;
 
         output |= (x_shift | y_shift | z_shift);
+        ---------------------------------------------*/
+        output |= (x & (static_cast<uint64_t>(1) << i)) << (i*2);
+        output |= (y & (static_cast<uint64_t>(1) << i)) << (i*2+1);
+        output |= (z & (static_cast<uint64_t>(1) << i)) << (i*2+2);
     }
     return output;
 }
--- src/p2b.cpp
+++ src/p2b.cpp
@@ -1,22 +1,173 @@
 #include "p2a.h"
 #include <iostream>
 #include <bitset>
+#include <vector>
+#include <random>
+#include "timer.h"
 
-void test() {
-    uint64_t test = 0b1101;
-    std::bitset<16> out(test);
-    std::cout << out << "\n";
-    uint64_t result = expand(test, 3);
-    std::bitset<16> out2(result);
-    std::cout << out2 << "\n";
-}
+#define M_SIZE 16
+#define STRIDE 4
 
-int main() { 
+void p2a_test() {
     uint64_t x = 1;     // 0001
     uint64_t y = 2;     // 0010
     uint64_t z = 4;     // 0100
-    uint16_t prod = morton3d(x,y,z);
+    uint64_t prod = morton3d(x,y,z);
     std::bitset<16> mort(prod);
     std::cout << mort << "\n";
+}
+
+// Row-major kernel
+std::vector<uint64_t> Ka(uint64_t x, uint64_t y, uint64_t z) {
+    std::vector<uint64_t> mat = std::vector<uint64_t>(x*y*z);
+    for(uint64_t i = 0; i < x; i++) {
+        for(uint64_t j = 0; j < y; j++) {
+            for(uint64_t k = 0; k < z; k++) {
+                uint64_t val = x*y*i + z*j + k;
+                mat[val] = i+j+k;   // Populate
+            }
+        }
+    }
+    return mat;
+}
+
+// Morton-order kernel
+std::vector<uint64_t> Kb(uint64_t x, uint64_t y, uint64_t z) {
+    auto mat = std::vector<uint64_t>(4*4*4);
+    for(uint64_t i = 0; i < x; i++) {
+        for(uint64_t j = 0; j < y; j++) {
+            for(uint64_t k = 0; k < z; k++) {
+                mat[morton3d(i,j,k)] = i+j+k;   // Populate
+            }
+        }
+    }
+    return mat;
+}
+
+size_t rmiA(size_t x, size_t y, size_t z) {
+    return x*M_SIZE*M_SIZE + y*M_SIZE + z;
+}
+
+size_t rmiK(size_t x, size_t y, size_t z) {
+    return x*STRIDE*STRIDE + y*STRIDE + z;
+}
+
+size_t rmiConv(size_t x, size_t y, size_t z) {
+    size_t width = M_SIZE/STRIDE;
+    return (x/STRIDE)*width*width + (y/STRIDE)*width + z/STRIDE;
+}
+
+int main() {
+    // Initialize contiguous 'matrices'
+    auto row_mat = std::vector<uint64_t>(M_SIZE*M_SIZE*M_SIZE);
+    auto morton_mat = std::vector<uint64_t>(M_SIZE*M_SIZE*M_SIZE);
+
+    // -----
+    // POPULATE
+    // -----
+
+    // Init rng
+    std::mt19937_64 rng(0);
+    // Row-major population
+    for(int i = 0; i < M_SIZE; i++) {
+        for(int j = 0; j < M_SIZE; j++) {
+            for(int k = 0; k < M_SIZE; k++) {
+                row_mat[M_SIZE*M_SIZE*i + M_SIZE*j + k] = rng();   // Populate
+            }
+        }
+    }
+
+    rng.seed(0);
+    // Morton population
+    for(int i = 0; i < M_SIZE; i++) {
+        for(int j = 0; j < M_SIZE; j++) {
+            for(int k = 0; k < M_SIZE; k++) {
+                morton_mat[morton3d(i,j,k)] = rng();   // Populate
+            }
+        }
+    }
+
+    // -----
+    // TEST
+    // -----
+    std::vector<uint64_t> kern_a = Ka(4,4,4);
+    std::vector<uint64_t> kern_b = Kb(4,4,4);
+
+    Timer stopwatch = Timer();
+    // Row-major convolution
+    uint64_t output_size = M_SIZE*M_SIZE*M_SIZE / (STRIDE*STRIDE*STRIDE);
+    auto rm_output = std::vector<uint64_t>(output_size);
+    for(size_t frame_x = 0; frame_x < M_SIZE; frame_x += STRIDE) {           // -----
+        for(size_t frame_y = 0; frame_y < M_SIZE; frame_y += STRIDE) {       // Iterate across frames
+            for(size_t frame_z = 0; frame_z < M_SIZE; frame_z += STRIDE) {   // -----
+                uint64_t sum = 0;
+                for(int x = 0; x < STRIDE; x++) {                       // -----
+                    for(int y = 0; y < STRIDE; y++) {                   // Iterate within frames
+                        for(int z = 0; z < STRIDE; z++) {               // -----
+                            sum += kern_a[rmiK(x, y, z)] * row_mat[rmiA(frame_x+x, frame_y+y, frame_z+z)];
+                        }
+                    }
+                }
+                rm_output[rmiConv(frame_x, frame_y, frame_z)] = sum;                              // Store convoluted frame
+            }
+        }
+    }
+    uint64_t rm_time = stopwatch.click<Timer::Micros>();    // Collect time (RM)
+/*
+    std::cout << "rm_output";
+    for(size_t i = 0; i < M_SIZE; i += STRIDE) {
+        std::cout << "\n";
+        for(size_t j = 0; j < M_SIZE; j += STRIDE) {
+            for(size_t k = 0; k < M_SIZE; k += STRIDE) {
+                std::cout << "(" << i/STRIDE << j/STRIDE << k/STRIDE << ")" << rm_output[rmiConv(i,j,k)] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+*/
+
+    // Morton Convolution
+    auto morton_output = std::vector<uint64_t>(output_size);
+    for(size_t frame_x = 0; frame_x < M_SIZE; frame_x += STRIDE) {           // -----
+        for(size_t frame_y = 0; frame_y < M_SIZE; frame_y += STRIDE) {       // Iterate across frames
+            for(size_t frame_z = 0; frame_z < M_SIZE; frame_z += STRIDE) {   // -----
+                uint64_t sum = 0;
+                for(size_t x = 0; x < STRIDE; x++) {            // -----
+                    for(size_t y = 0; y < STRIDE; y++) {        // Iterate within frames
+                        for(size_t z = 0; z < STRIDE; z++) {    // -----
+                            sum += kern_b[morton3d(x,y,z)] * morton_mat[(frame_x+x, frame_y+y, frame_z+z)];
+                        }
+                    }
+                }
+                morton_output[rmiConv(frame_x, frame_y, frame_z)] = sum;
+            }
+        }
+    }
+    uint64_t morton_time = stopwatch.glance<Timer::Micros>();   // Collect time (CM)
+
+
+    std::cout << "morton_output";
+    for(size_t i = 0; i < M_SIZE; i += STRIDE) {
+        std::cout << "\n";
+        for(size_t j = 0; j < M_SIZE; j += STRIDE) {
+            for(size_t k = 0; k < M_SIZE; k += STRIDE) {
+                std::cout << "(" << i/STRIDE << j/STRIDE << k/STRIDE << ")" << morton_output[rmiConv(i,j,k)] << " ";
+            }
+            std::cout << "\n";
+        }
+    }
+
+    for(size_t i = 0; i < M_SIZE; i += STRIDE) {
+        for(size_t j = 0; j < M_SIZE; j += STRIDE) {
+            for(size_t k = 0; k < M_SIZE; k += STRIDE) {
+                // assert(rm_output[rmiConv(i,j,k)] == morton_output[morton3d(i,j,k)]);
+            }
+        }
+    }
+
+
+    std::cout << "Row-major " << rm_time << "ms\n";
+    std::cout << "Morton    " << morton_time << "ms\n";
+
     return 0; 
 }
